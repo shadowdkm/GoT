d0ce289c76e7f05b8f772e62d310dcdcec3b893a
shadowdkm <shadowdkm@gmail.com>
2016-07-04 17:55:18 +0800

    random good with label

--- MARCH_ORDER_ELEMENT.m
+++ MARCH_ORDER_ELEMENT.m
@@ -1,12 +1,12 @@
 classdef MARCH_ORDER_ELEMENT < handle
     properties (GetAccess=public, SetAccess=private)
       target=0;
-      troop_indexes=[0,0,0,0];
+      troop_type_array=[];
     end
     methods
-       function obj = MARCH_ORDER_ELEMENT(target,troop_index)
+       function obj = MARCH_ORDER_ELEMENT(target,troop_type_array)
             obj.target=target;
-            obj.troop_indexes=troop_index;           
+            obj.troop_type_array=troop_type_array;           
        end      
     end
 end

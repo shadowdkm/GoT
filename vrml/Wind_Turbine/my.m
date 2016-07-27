af9bad783c26434898c6ac56bd6324afff4ffa65
shadowdkm <shadowdkm@gmail.com>
2016-07-27 18:03:49 +0800

    plot3dinmatlab

--- vrml/Wind_Turbine/my.m
+++ vrml/Wind_Turbine/my.m
@@ -1,15 +1,14 @@
 clear
 clc
-mp=vrworld('./dddeeerrr.wrl');
+mp=vrworld('./testing.wrl');
 open(mp);
 view(mp);
 
 % mynode = vrnode(mp, 'sroot','Transform');
 
-
 for i=1:10
 mynode = vrnode(mp.sroot, 'children',['sball' num2str(randi(1000))],'Inline');
-mynode.url='dddeeerrr0.wrl';    
+mynode.url='axis_simple.x3d';    
     
 mp.sroot.translation=randi(100,[1,3])/20;
 mp.sroot.scale=randi(200,[1,3])/100;
@@ -17,7 +16,6 @@ pause(0.01)
 
 end
 
-save(mp,'addedball.wrl')
 save(mp,'addedball.x3d')
 
 close(mp)
